ca78e4fd4ce4dc29014c05689c21ab5384e5cf3d
hackrole <daipeng123456@gmail.com>
2014-12-11 10:21:26 +0800

    finish btshare

--- dmbt/run/get.py
+++ dmbt/run/get.py
@@ -2,15 +2,41 @@
 # encoding: utf-8
 
 import json
-from os import path
+import uuid
+import gevent
+import os
+from gevent import monkey
+from gevent import queue
+
+monkey.patch_all()
 
 
-#fp = path.join(
-    #path.dirname(path.realpath(__file__)),
-    #"btshare.json"
-#)
+fp = "../btshare.json"
+f = open(fp)
+data = json.load(f)
 
-#text = open(fp).read()
-#json.loads(text)
 
-test_url = "http://www.btshare.net/show-4180e38b1727f38794cabd3f3f440c531bf31eb7.html"
+q = queue.JoinableQueue()
+
+for i in data:
+    d = {
+        'bt_url': i['bt_url'],
+        'title': str(uuid.uuid1()),
+    }
+    q.put(d)
+
+
+def worker():
+    while True:
+        item = q.get()
+        try:
+            fn = '.'.join([item['title'], "torrent"])
+            os.system('wget "%s" -O %s' % (item['bt_url'], fn))
+        finally:
+            q.task_done()
+
+
+for i in range(20):
+    gevent.spawn(worker)
+
+q.join()
